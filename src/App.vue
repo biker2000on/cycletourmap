ebd9d53c6ee155adb6415261ad67bb193b456070
biker2000on <bikerhiker10@gmail.com>
2019-07-22 19:32:38 +0600

    setting up sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,57 @@
+
 <template>
-  <div id="app">
-    <Toggle id="toggle"/>
-    <Leaflet />
-    <Summary />
-    <h2>All Rides</h2>
-    <rides />
-  </div>
+  <v-app id="app">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item @click>
+          <v-list-item-action>
+            <v-icon>home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click>
+          <v-list-item-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex text-center>
+            <div class="inline">
+              <input type="checkbox" v-model="isMetric" />
+              {{ isMetric ? "Units(Switch to Standard)" : 'Units(Switch to Metric)'}}
+              <input
+                type="checkbox"
+                v-model="markersOn"
+              /> Markers
+              <input type="checkbox" v-model="polylinesOn" /> Lines
+            </div>
+            <Leaflet :markersOn="markersOn" :polylinesOn="polylinesOn" />
+            <Summary :isMetric="isMetric" />
+            <h2>All Rides</h2>
+            <rides :isMetric="isMetric" />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
@@ -21,6 +67,14 @@ export default {
   components: {
     Leaflet, Summary, Toggle, Rides
   },
+  data() {
+    return {
+      markersOn: true,
+      polylinesOn: true,
+      isMetric: false,
+      drawer: null,
+    }
+  }
 }
 </script>
 
