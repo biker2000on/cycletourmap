02a073a4e79d0c2f18dec0026f6832a14bc7b142
biker2000on <bikerhiker10@gmail.com>
2019-09-28 09:43:43 +0300

    fix for Strava menus

--- src/components/Strava.vue
+++ src/components/Strava.vue
@@ -26,7 +26,6 @@
           v-model="menu"
           transition="scale-transition"
           offset-y
-          full-width
           :nudge-right="40"
           max-width="290px"
           min-width="290px"
@@ -52,7 +51,6 @@
           v-model="menu2"
           transition="scale-transition"
           offset-y
-          full-width
           :nudge-right="40"
           max-width="290px"
           min-width="290px"
